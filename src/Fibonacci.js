d05d4103216c2cec1f355ae14654bd75f61d1682
zlargon <zlargon@icloud.com>
2016-04-05 17:37:55 +0800

    implement Fibonacci with ES6 generator

---
+++ src/Fibonacci.js
@@ -0,0 +1,19 @@
+"use strict";
+
+function * fibonacci () {
+  let pre = 0, cur = 1;
+  yield pre;
+  yield cur;
+  for (;;) {
+    [pre, cur] = [cur, pre + cur];
+    yield cur;
+  }
+}
+
+module.exports = function (n) {
+  var f = fibonacci();
+  for (var i = 0; i < n; i++) {
+    f.next();
+  }
+  return f.next().value;
+};
---
+++ src/index.js
@@ -0,0 +1,5 @@
+"use strict";
+
+module.exports = {
+  Fibonacci: require('./Fibonacci')
+};
